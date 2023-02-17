63dd39a8d69d28e3ce3378d22642668b1ae12dcc
ANZY KENYA <mugokevin900@gmail.com>
2023-02-17 19:28:41 +0300

    first commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,37 +1,80 @@
-const express =  require('express');
+const e = require('express');
+const express = require('express');
 const app = express();
 
-const {products} = require('./data');
+let {people} = require('./data');
 
-app.get('/' , (req,res)=>{
- res.send('<h1>Home page </h1> <a href="/api/products" > Products</a>' );
-});
+//stattic assets.
+app.use(express.static('./methods-public'));
 
-app.get('/api/products' , (req,res)=>{
- const newProducts = products.map((product ) => {
-   const {id,name,image} = product;
-   return {id,name,image};
- })
- res.json(newProducts);
+//parse form data
+app.use(express.urlencoded({extended:false}));
+
+//parse Json
+app.use(express.json());
+
+app.get('/api/people' , (req,res) =>{
+  res.status(200).json({success:true , data:people});
 })
 
-  app.get('/api/products/:productID' , (req,res)=>{
-
-   //console.log(req);
-   //console.log(req.params);
-
-   const {productID} = req.params;
-
-   const singleProduct = products.find((product) => 
-      product.id === Number(productID)
-   )
-  if(!singleProduct){
-   return res.status(404).send('Product not found');
+app.post('/api/people', (req,res)=>{
+  const {name} = req.body;
+  if(!name){
+    return res.status(400).json({success:false , msg:'Please provide name value'})
   }
-    return res.json(singleProduct);
-  })
-  
+  res.status(201).send({success:true,person:name });
+})
 
-app.listen(5000 ,()=>{
-   console.log('Server listening to port 5000...')
+app.post('/api/postman/people', (req,res)=>{
+  const {name} = req.body;
+  if(!name){
+    return res.status(400).json({success:false , msg:'Please provide name value'})
+  }
+  res.status(201).send({success:true, data:[...people,name]});
+})
+
+app.post('/login' , (req,res)=>{
+ // console.log(req.body);
+
+ const {name} = req.body;
+ if(name){
+  return res.status(200).send(`Welcome ${name}`);
+ }
+  res.status(401).send('Please provide credentials');
+})
+
+app.put('/api/people/:id', (req,res)=>{
+  const {id} = req.params;
+  const {name} = req.body;
+  console.log(id , name);
+  
+  const person = people.find((person) => person.id === Number(id));
+
+  if(!person){
+    return res.status(400).json({success:false , msg:`No person with id ${id}`})
+  }
+
+  const newPeople = people.map((person)=>{
+    if(person.id === Number(id)){
+      person.name = name
+    }
+    return person;
+  })
+ res.status(200).json({success:true , data:newPeople})
+})
+
+app.delete('/api/people/:id', (req,res) => {
+
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if(!person){
+    return res.status(400).json({success:false , msg:`No person with id ${req.params.id}`})
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+  return res.status(200).json({success:true , data:newPeople})
+
+})
+app.listen(5000 , ()=>{
+  console.log("Server listening to port 5000...")
 });
